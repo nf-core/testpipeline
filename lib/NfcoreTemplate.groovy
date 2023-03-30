0a56d1edd153fadb6267850c83f225e461721357
Rob Syme <rob.syme@gmail.com>
2023-03-30 15:18:34 +0100

    Remove branded if statement

--- lib/NfcoreTemplate.groovy
+++ lib/NfcoreTemplate.groovy
@@ -322,12 +322,12 @@ class NfcoreTemplate {
         String workflow_version = NfcoreTemplate.version(workflow)
         String.format(
             """\n
-            ${dashedLine(monochrome_logs)}{% if branded %}
+            ${dashedLine(monochrome_logs)}
                                                     ${colors.green},--.${colors.black}/${colors.green},-.${colors.reset}
             ${colors.blue}        ___     __   __   __   ___     ${colors.green}/,-._.--~\'${colors.reset}
             ${colors.blue}  |\\ | |__  __ /  ` /  \\ |__) |__         ${colors.yellow}}  {${colors.reset}
             ${colors.blue}  | \\| |       \\__, \\__/ |  \\ |___     ${colors.green}\\`-._,-`-,${colors.reset}
-                                                    ${colors.green}`._,._,\'${colors.reset}{% endif %}
+                                                    ${colors.green}`._,._,\'${colors.reset}
             ${colors.purple}  ${workflow.manifest.name} ${workflow_version}${colors.reset}
             ${dashedLine(monochrome_logs)}
             """.stripIndent()
